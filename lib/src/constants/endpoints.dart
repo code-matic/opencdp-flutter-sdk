02bd15b4c5650a6a88bdce3466d8e68cd191a114
ueh-kabari, yereka <yereka@chigisoft.com>
2025-07-18 15:19:37 +0100

    chore: Update base URL for CDP API in endpoints.dart
    - Changed the base URL for the CDP API from the previous gateway to the new API endpoint at 'https://api.opencdp.io/gateway'. This update ensures that the SDK points to the correct service for data access.

--- lib/src/constants/endpoints.dart
+++ lib/src/constants/endpoints.dart
@@ -1,8 +1,7 @@
 /// API endpoints for the Open CDP SDK
 class CDPEndpoints {
   /// Base URL for the CDP API
-  static const String baseUrl =
-      'https://cdp-data-gateway-749119130796.europe-west1.run.app/data-gateway';
+  static const String baseUrl = 'https://api.opencdp.io/gateway';
 
   static const String version = '/v1';
 
