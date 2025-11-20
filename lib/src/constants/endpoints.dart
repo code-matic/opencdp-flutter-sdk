7f57b04f664eb036dfcff22d797cd89f9b56889b
codeflames <yereka@codematic.io>
2025-11-20 12:43:38 +0100

    chore: update CDP API base URL and add comments for production and staging endpoints

--- lib/src/constants/endpoints.dart
+++ lib/src/constants/endpoints.dart
@@ -1,8 +1,14 @@
 /// API endpoints for the Open CDP SDK
 class CDPEndpoints {
   /// Base URL for the CDP API
-  static const String baseUrl = 'https://api.opencdp.io/gateway/data-gateway';
+  static const String baseUrl =
+      'https://cdp-data-gateway-prod1-81240184175.europe-west1.run.app/data-gateway';
 
+  /// production url
+// https://api.opencdp.io/gateway/data-gateway/v1 //
+
+  /// staging url
+// https://cdp-data-gateway-prod1-81240184175.europe-west1.run.app/data-gateway/v1 //
   static const String version = '/v1';
 
   /// Identify a person
